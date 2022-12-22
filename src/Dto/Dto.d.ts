1db9f50831d1ff5f0c4b3b6ebea376db840463da
byungmin12 <kbm940526@naver.com>
2022-12-22 17:29:04 +0900

    #5 issue-5-request-api
    서버 응답 타입 설정

---
+++ src/Dto/Dto.d.ts
@@ -0,0 +1,1 @@
+export interface IProject {    id: number,    title: string,    description: string,    price: number,    discountPercentage: number,    rating: number,    stock: number,    brand: string,    category: string,    thumbnail: string,    images: string[]}export interface IDataDto {    projects: IProject[];    total: number;    skip: number;    limit: number;}
