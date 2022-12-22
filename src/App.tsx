19b42ccbe53f57f3b304a4d4dad61d27ee3e56a3
byungmin12 <kbm940526@naver.com>
2022-12-22 17:28:49 +0900

    #5 issue-5-request-api
    react-query setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import './App.css';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
 import Search from "./components/Search";
 import Table from "./components/Table";
 
+
 function App() {
-  return (
-    <div className="App">
-      <Search />
-        <Table />
-    </div>
-  );
+    const queryClient = new QueryClient()
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <div className="App">
+                <Search/>
+                <Table/>
+            </div>
+        </QueryClientProvider>
+
+
+    );
 }
 
 export default App;
